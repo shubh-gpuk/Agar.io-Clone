c66b9eface714ca9c526fa0bd6233a0fb2828584
shubg-gpuk <shubh.gpuk@gmail.com>
2020-11-03 08:38:14 +0530

    Send orbs location to client.

--- sockets/socketMain.js
+++ sockets/socketMain.js
@@ -1,5 +1,21 @@
 //All socket logic of server
+
 const io = require('../servers').io;
 
+const Orb = require('./classes/Orb')
+let orbs = []
+
+initGame();
+
+io.on('connect', (socket) => {
+    socket.emit('init', {orbs});
+})
+
+//Run at game start
+function initGame(){
+    for(i = 0; i < 500; i++){
+        orbs.push(new Orb());
+    }
+}
 
 module.exports = io;
